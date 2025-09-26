c01290fba71d6e5ec057fab89ba2a335c1c6602f
Johanan Seeruthen <bob@outlook.com>
2025-09-26 12:51:42 +0100

    Added update and delete

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ users: list[User] = []
 @app.get("/hello")
 def hello():
     return {"message": "Hello, World!"}
-    
+
 @app.get("/api/users")
 def get_users():
     return users
@@ -26,3 +26,28 @@ def add_user(user: User):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id already exists")
     users.append(user)
     return user
+    
+# Practice Exercises
+#Add a PUT endpoint to update and existing user
+#Add a Delete endpoint to Delete and existing user
+#Add Health endpoint
+# Add a student id to the User class that must start with S followed by exactly 7 digits
+
+@app.put("/api/users/{user_id}", status_code=status.HTTP_201_CREATED)
+def update_user(user_id: int, updated_user: User):
+    for i, u in enumerate(users):
+        if u.user_id == user_id:  
+            users[i] = updated_user
+            return updated_user
+    if any(u.user_id == user.user_id for u in users):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id already exists")
+
+@app.delete("/api/users", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int):
+    for u in users:
+        if u.user_id == user_id:  
+            users.remove(u)
+            return u
+    if any(u.user_id == user.user_id for u in users):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id already exists")
+    
